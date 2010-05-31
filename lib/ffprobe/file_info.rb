2a96c4a0c29cb08fc6f5df1edde0c8f4f0345007
kingpong <philip@pastemagazine.com>
2010-05-30 20:30:13 -0400

    accessors for FileInfo

--- lib/ffprobe/file_info.rb
+++ lib/ffprobe/file_info.rb
@@ -1,4 +1,8 @@
 class FFProbe
   class FileInfo
+    
+    attr_reader :filename, :nb_streams, :demuxer_name, :demuxer_long_name,
+                :start_time, :duration, :size, :bit_rate
+    
   end
 end
